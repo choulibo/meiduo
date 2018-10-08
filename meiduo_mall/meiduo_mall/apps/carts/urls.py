a5233d1d4756f21dac74695e157439c505f356c1
test <306191816@qq.com>
2018-10-09 01:09:52 +0800

    保存购物车前端逻辑实现

---
+++ meiduo_mall/meiduo_mall/apps/carts/urls.py
@@ -0,0 +1,10 @@
+# coding = utf-8
+from django.conf.urls import url
+
+from . import views
+
+# from meiduo_mall.apps.users import views
+urlpatterns = [
+    url(r'^cart/$', views.CartsView.as_view()),  # 列表展示
+
+]
