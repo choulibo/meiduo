bf7430412c98b2e2b370d0703c1defb16f76887e
test <306191816@qq.com>
2018-10-11 18:56:42 +0800

    确认订单后端逻辑实现

---
+++ meiduo_mall/meiduo_mall/apps/orders/serializers.py
@@ -0,0 +1,21 @@
+# coding = utf-8
+from rest_framework import serializers
+
+from goods.models import SKU
+
+
+class CartSKUSerializer():
+    """购物车商品序列化器"""
+    count = serializers.IntegerField(label='数量')
+
+    class Meta:
+        model = SKU
+        fields = ('id','name','default_image_url','price','count')
+
+class OrderSettlementSerializer(serializers.Serializer):
+    """
+    订单结算数据序列化器
+    """
+    freight = serializers.DecimalField(label='运费', max_digits=10, decimal_places=2)
+    skus = CartSKUSerializer(many=True)
+
--- meiduo_mall/meiduo_mall/apps/orders/views.py
+++ meiduo_mall/meiduo_mall/apps/orders/views.py
@@ -1,8 +1,55 @@
+from _decimal import Decimal
+
 from django.shortcuts import render
-
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+from django_redis import get_redis_connection
+from rest_framework.response import Response
+
+from carts.serializers import CartSKUSerializer
+from goods.models import SKU
+from orders.serializers import OrderSettlementSerializer
 
 
+class OrderSettlementView():
+    """
+    订单结算
+    """
+    permission_class = [IsAuthenticated]
+    def get(self,request):
+        # 获取用户对象
+        user = request.user
+        # 从redis中查询购物车 SKU_id count selected
+        redis_conn = get_redis_connection('cart')
+        # hash 商品数量
+        redis_cart_dict = redis_conn.hgetall('cart_%s' %user.id)
+        # set 勾选状态
+        redis_cart_selected = redis_conn.smembers('cart_selected_%s' %user.id)
+
+        cart = {}
+        # cart = {
+        #   勾选商品信息
+        #   sku_id :count
+        # }
+        # 对勾选商品遍历添加到cart字典中
+        for sku_id in redis_cart_selected:
+            cart[int(sku_id)] = int(redis_cart_selected[sku_id])
+
+        # 查询数据库中其他字段
+        # 获取所有勾选状态的商品sku_id ,并得到该对象
+        sku_id_list = cart.keys()
+        sku_obj_list = SKU.objects.filter(id__in = sku_id_list)
+        for sku in sku_obj_list:
+            sku.count = cart[sku_id]
+
+        # 运费
+        freight = Decimal('10.00')
+        # 序列化返回
+        # serializer = CartSKUSerializer(sku_obj_list,many=True)
+        # return Response({'freight':freight,'skus':sku_id})
+
+        serializer = OrderSettlementSerializer({'freight':freight,'skus':sku_obj_list})
+        return Response(serializer.data)
 
 
 
