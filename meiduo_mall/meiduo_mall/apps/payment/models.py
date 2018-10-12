34be664b20ca3180b8ba04ee7d6e27064b6dc787
test <306191816@qq.com>
2018-10-12 12:55:45 +0800

    创建支付子应用及配置

--- meiduo_mall/meiduo_mall/apps/payment/models.py
+++ meiduo_mall/meiduo_mall/apps/payment/models.py
@@ -10,8 +10,8 @@ class Payment(BaseModel):
     支付信息
     """
 
-    order = models.ForeignKey(OrderInfo,on_delete=models.CASCADE,verbose_name='订单')
-    trade_id = models.CharField(max_length=100,unique=True,null=True,blank=True,verbose_name='支付编号')
+    order = models.ForeignKey(OrderInfo, on_delete=models.CASCADE, verbose_name='订单')
+    trade_id = models.CharField(max_length=100, unique=True, null=True, blank=True, verbose_name='支付编号')
 
     class Meta:
         db_table = 'tb_payment'
